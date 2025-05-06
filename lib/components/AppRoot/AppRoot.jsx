0ede83413a98ead4e6475c84103d4cacefb96a22
Tim Goyer <tim.goyer@alliancebernstein.com>
2025-05-06 15:15:39 -0500

    Change to use UnHead for head maintenance.

--- lib/components/AppRoot/AppRoot.jsx
+++ lib/components/AppRoot/AppRoot.jsx
@@ -1,12 +1,17 @@
 import { useEffect, useRef } from 'react';
 import { ThemeProvider, useTheme } from '@/components';
-
+import { UnheadProvider, createHead } from '@unhead/react/client';
+import { useHead } from '@unhead/react';
 import './AppRoot.css';
+
+const head = createHead();
 
 const AppRoot = ({ children, ...props }) => {
     return (
         <ThemeProvider {...props}>
-            <AppRootContainer>{children}</AppRootContainer>
+            <UnheadProvider head={head}>
+                <AppRootContainer>{children}</AppRootContainer>
+            </UnheadProvider>
         </ThemeProvider>
     );
 };
@@ -25,21 +30,27 @@ const AppRootContainer = ({ children }) => {
         }
     }, [themeClass]);
 
-    return (
-        <>
-            {children}
-            <link rel="preconnect" href="https://fonts.googleapis.com" precedence="high" />
-            <link rel="preconnect" href="https://fonts.gstatic.com" precedence="high" />
-            <link
-                href="https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap"
-                precedence="high"
-                rel="stylesheet"
-            />
-            <link
-                href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.7.2/css/all.min.css"
-                precedence="high"
-                rel="stylesheet"
-            />
-        </>
-    );
+    useHead({
+        htmlAttrs: { lang: 'en-US' },
+        link: [
+            {
+                rel: 'preconnect',
+                href: 'https://fonts.googleapis.com',
+            },
+            {
+                rel: 'preconnect',
+                href: 'https://fonts.gstatic.com',
+            },
+            {
+                rel: 'stylesheet',
+                href: 'https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap',
+            },
+            {
+                rel: 'stylesheet',
+                href: 'https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.7.2/css/all.min.css',
+            },
+        ],
+    });
+
+    return children;
 };
