f26cae0e299c2dc22073a2d04b7fa53509ac5f63
Tim Goyer <tim.goyer@alliancebernstein.com>
2025-05-06 14:20:09 -0500

    Remove hacky injection and implement React 19 metadata handling.

--- lib/components/AppRoot/AppRoot.jsx
+++ lib/components/AppRoot/AppRoot.jsx
@@ -19,40 +19,27 @@ const AppRootContainer = ({ children }) => {
     const docRef = useRef(window?.document);
 
     useEffect(() => {
-        const doc = docRef.current;
         const body = docRef.current?.body;
-        const linkFragment = getFontLinks(doc);
         if (body != null) {
             body.className = themeClass;
-            body.appendChild(linkFragment);
         }
     }, [themeClass]);
 
-    return children;
+    return (
+        <>
+            {children}
+            <link rel="preconnect" href="https://fonts.googleapis.com" precedence="high" />
+            <link rel="preconnect" href="https://fonts.gstatic.com" precedence="high" />
+            <link
+                href="https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap"
+                precedence="high"
+                rel="stylesheet"
+            />
+            <link
+                href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.7.2/css/all.min.css"
+                precedence="high"
+                rel="stylesheet"
+            />
+        </>
+    );
 };
-
-function getFontLinks(doc) {
-    if (doc != null) {
-        const links = [
-            { rel: 'preconnect', href: 'https://fonts.googleapis.com' },
-            { rel: 'preconnect', href: 'https://fonts.gstatic.com' },
-            {
-                rel: 'stylesheet',
-                href: 'https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap',
-            },
-            {
-                rel: 'stylesheet',
-                href: 'https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.7.2/css/all.min.css',
-            },
-        ];
-        const fragment = document.createDocumentFragment();
-        links.forEach((l) => {
-            const link = doc.createElement('link');
-            link.rel = l.rel;
-            link.href = l.href;
-            fragment.appendChild(link);
-        });
-        return fragment;
-    }
-    return null;
-}
